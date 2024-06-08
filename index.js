47d621b3d56c0720ff76f77d819128d0218a696d
Matt Bryant <brybrant@gmail.com>
2024-06-09 00:46:32 +0100

    v1.0.3

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-import * as path from 'node:path';
-import * as fs from 'node:fs';
+import { basename } from 'node:path';
+import { mkdir, writeFileSync } from 'node:fs';
 
-import postCSSConfig from './configs/postcss.config.js';
-import purgeCSSConfig from './configs/purgecss.config.js';
-import stylelintConfig from './configs/stylelint.config.js';
-import svgoConfig from './configs/svgo.config.js';
+export { default as postCSSConfig } from './configs/postcss.config.js';
+export { default as purgeCSSConfig } from './configs/purgecss.config.js';
+export { default as stylelintConfig } from './configs/stylelint.config.js';
+export { default as svgoConfig } from './configs/svgo.config.js';
 
 /**
  * https://github.com/madyankin/postcss-modules
  * @param {String} [cssFileName] - Name of the CSS file
  * @param {Object.<String, String>} [json] - Transformed classes
  */
-function exportCSSModuleJSON(cssFileName, json) {
-  const module = path.basename(cssFileName, '.scss');
+export function exportCSSModuleJSON(cssFileName, json) {
+  const module = basename(cssFileName, '.scss');
 
-  fs.mkdir('./modules', {recursive: true}, (error) => {
+  mkdir('./modules', {recursive: true}, (error) => {
     if (error) throw error;
 
-    fs.writeFileSync(`./modules/${module}.json`, JSON.stringify(json));
+    writeFileSync(`./modules/${module}.json`, JSON.stringify(json));
   });
-};
-
-export {
-  exportCSSModuleJSON,
-  postCSSConfig,
-  purgeCSSConfig,
-  stylelintConfig,
-  svgoConfig,
 };
