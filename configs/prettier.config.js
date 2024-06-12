ec14857eb3b6d22c506e7bf61a5988e8aa602a36
Matt Bryant <brybrant@gmail.com>
2024-06-12 16:58:39 +0100

    prettier config

---
+++ configs/prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * https://prettier.io/docs/en/options
+ * @type {import('prettier').Config}
+ */
+export default {
+  bracketSpacing: false,
+  endOfLine: 'auto',
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
