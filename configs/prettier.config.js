a8ab195e773ff1f8198d8a8c5e49b2eaf5bba4d8
Matt Bryant <brybrant@gmail.com>
2024-06-13 03:19:32 +0100

    prettier svelte

--- configs/prettier.config.js
+++ configs/prettier.config.js
@@ -5,6 +5,15 @@
 export default {
   bracketSpacing: false,
   endOfLine: 'auto',
+  overrides: [
+    {
+      files: '*.svelte',
+      options: {
+        parser: 'svelte',
+      },
+    },
+  ],
+  plugins: ['prettier-plugin-svelte'],
   singleQuote: true,
   jsxSingleQuote: true,
 };
