0288ba8eb1eb7341ba1f6b3ef40d52dec8b377bd
Matt Bryant <brybrant@gmail.com>
2024-08-04 05:03:57 +0100

    Export cssnano config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as cssModulesExportJSON } from './configs/cssmodules.js';
+export { default as cssnanoConfig } from './configs/cssnano.config.js';
 export { default as eslintConfig } from './configs/eslint.config.js';
 export { default as postCSSConfig } from './configs/postcss.config.js';
 export { default as prettierConfig } from './configs/prettier.config.js';
