8351e4b6ab130bc24e15706e09eafe5af59f11ce
Matt Bryant <brybrant@gmail.com>
2024-08-10 15:31:17 +0100

    Update eslint config

--- configs/eslint.config.js
+++ configs/eslint.config.js
@@ -38,83 +38,94 @@ function mergeDeep(target, ...sources) {
   }, {});
 }
 
-/**
- * @typedef {import('eslint').Linter.Config} ESLintConfig
- * @typedef {import('eslint').Linter.BaseConfig} ESLintBaseConfig
- */
+/** @typedef {import('eslint').Linter.BaseConfig} ESLintConfig */
+
+/** @type {ESLintConfig} */
+const baseConfig = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
+
+/** @type {ESLintConfig} */
+const jsdocConfig = {
+  plugins: [
+    'jsdoc',
+  ],
+  rules: {
+    'jsdoc/check-alignment': 1,
+    'jsdoc/check-indentation': 1,
+    'jsdoc/check-param-names': 1,
+    'jsdoc/check-property-names': 1,
+    'jsdoc/check-syntax': 1,
+    'jsdoc/check-tag-names': 1,
+    'jsdoc/check-types': 1,
+    'jsdoc/check-values': 1,
+    'jsdoc/empty-tags': 1,
+    'jsdoc/implements-on-classes': 1,
+    'jsdoc/multiline-blocks': 1,
+    'jsdoc/no-blank-block-descriptions': 1,
+    'jsdoc/no-blank-blocks': 1,
+    'jsdoc/no-defaults': 1,
+    'jsdoc/no-multi-asterisks': [1, {'allowWhitespace': true}],
+    'jsdoc/no-undefined-types': 1,
+    'jsdoc/require-asterisk-prefix': 1,
+    'jsdoc/require-jsdoc': 1,
+    'jsdoc/require-param': 1,
+    'jsdoc/require-param-description': 1,
+    'jsdoc/require-param-name': 1,
+    'jsdoc/require-param-type': 1,
+    'jsdoc/require-property': 1,
+    'jsdoc/require-property-description': 1,
+    'jsdoc/require-property-name': 1,
+    'jsdoc/require-property-type': 1,
+    'jsdoc/require-returns': 1,
+    'jsdoc/require-returns-check': 1,
+    'jsdoc/require-returns-type': 1,
+    'jsdoc/require-yields': 1,
+    'jsdoc/require-yields-check': 1,
+    'jsdoc/tag-lines': 1,
+    'jsdoc/valid-types': 1,
+  },
+  settings: {
+    jsdoc: {
+      tagNamePreference: {
+        property: 'prop',
+        augments: 'extends',
+      },
+    },
+  },
+};
 
 /**
  * https://eslint.org/docs/latest/integrate/nodejs-api#-new-eslintoptions
  * @param {ESLintConfig|ESLintConfig[]} [config] - ESLint config object(s)
- * @returns {ESLintBaseConfig}
+ * @returns {ESLintConfig}
  */
 export default (config) => {
-  /** @type {ESLintBaseConfig} */
-  const baseConfig = {
-    env: {
-      browser: true,
+  return mergeDeep(
+    baseConfig,
+    config || {},
+    jsdocConfig,
+    {
+      extends: [
+        'prettier',
+      ],
+      plugins: [
+        'prettier',
+      ],
+      rules: {
+        'prettier/prettier': ['error', prettierConfig],
+      }
     },
-    extends: [
-      'eslint:recommended',
-      'prettier',
-    ],
-    plugins: [
-      'jsdoc',
-      'prettier',
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      'prettier/prettier': ['error', prettierConfig],
-      'jsdoc/check-alignment': 1,
-      'jsdoc/check-indentation': 1,
-      'jsdoc/check-param-names': 1,
-      'jsdoc/check-property-names': 1,
-      'jsdoc/check-syntax': 1,
-      'jsdoc/check-tag-names': 1,
-      'jsdoc/check-types': 1,
-      'jsdoc/check-values': 1,
-      'jsdoc/empty-tags': 1,
-      'jsdoc/implements-on-classes': 1,
-      'jsdoc/multiline-blocks': 1,
-      'jsdoc/no-blank-block-descriptions': 1,
-      'jsdoc/no-blank-blocks': 1,
-      'jsdoc/no-defaults': 1,
-      'jsdoc/no-multi-asterisks': [1, {'allowWhitespace': true}],
-      'jsdoc/no-undefined-types': 1,
-      'jsdoc/require-asterisk-prefix': 1,
-      'jsdoc/require-jsdoc': 1,
-      'jsdoc/require-param': 1,
-      'jsdoc/require-param-description': 1,
-      'jsdoc/require-param-name': 1,
-      'jsdoc/require-param-type': 1,
-      'jsdoc/require-property': 1,
-      'jsdoc/require-property-description': 1,
-      'jsdoc/require-property-name': 1,
-      'jsdoc/require-property-type': 1,
-      'jsdoc/require-returns': 1,
-      'jsdoc/require-returns-check': 1,
-      'jsdoc/require-returns-type': 1,
-      'jsdoc/require-yields': 1,
-      'jsdoc/require-yields-check': 1,
-      'jsdoc/tag-lines': 1,
-      'jsdoc/valid-types': 1,
-    },
-    settings: {
-      jsdoc: {
-        tagNamePreference: {
-          property: 'prop',
-          augments: 'extends',
-        },
-      },
-    },
-  };
-
-  if (!config || typeof config !== 'object') return baseConfig;
-
-  return mergeDeep(baseConfig, config);
+  );
 };
 
 // export default (config) => ([
